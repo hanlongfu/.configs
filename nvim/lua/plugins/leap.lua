75817efc5982e17e6b4d5dd81282915f5d8f8c15
hanlongfu <jack.hanlong@gmail.com>
2024-09-16 09:54:20 -0500

    added leap highlighting

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -3,5 +3,11 @@ return {
 	config = function()
 		local leap = require("leap")
 		leap.create_default_mappings()
+		vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" }) -- or some grey
+		vim.api.nvim_set_hl(0, "LeapMatch", {
+			fg = "white",
+			bold = true,
+			nocombine = true,
+		})
 	end,
 }
