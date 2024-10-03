b68255a148a589e58928dd6d812649fbc5a947fe
hanlongfu <jack.hanlong@gmail.com>
2024-10-03 08:44:42 -0500

    Added neogit

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -4,5 +4,13 @@ return {
 		"nvim-lua/plenary.nvim", -- required
 		"sindrets/diffview.nvim", -- optional - Diff integration
 	},
-	config = true,
+	config = function()
+		local neogit = require("neogit")
+		neogit.setup({
+			integrations = {
+				telescope = true,
+				diffview = true,
+			},
+		})
+	end,
 }
