5d4551a93a6778f4d1e93b3239dc464f20cc196d
hanlongfu <jack.hanlong@gmail.com>
2025-05-20 09:09:12 -0500

    moved ideavimrc

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 -- font
-config.font = wezterm.font("NotoSansM Nerd Font Mono")
+config.font = wezterm.font("MesloLGM Nerd Font Mono")
 config.font_size = 13
 
 --window
