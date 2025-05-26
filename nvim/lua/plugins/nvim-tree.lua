cd505cc4e069d013da8d382cefd1ba229a17d1d2
hanlongfu <jack.hanlong@gmail.com>
2025-05-25 22:09:06 -0500

    changed keymap for nvim-tree config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -3,37 +3,40 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local nvimtree = require("nvim-tree")
-
     -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
-
     -- change color for arrows in tree to light blue
     vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
     vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
     vim.cmd([[ highlight SignColumn guibg=NONE]])
 
-    local function on_attach(bufnr)
+    local function my_on_attach(bufnr)
       local api = require("nvim-tree.api")
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
 
-      -- Buffer-specific keymaps (only active in nvim-tree buffer)
-      local opts = { buffer = bufnr, noremap = true, silent = true }
+      -- default mappings
+      api.config.mappings.default_on_attach(bufnr)
 
+      -- custom mappings
       vim.keymap.set("n", "<leader>m", function()
         api.tree.marks.bulk.move()
-      end, vim.tbl_extend("force", opts, { desc = "Move bookmarked file" }))
+      end, opts("Move bookmarked file"))
 
       vim.keymap.set("n", "<leader>c", function()
         api.tree.change_root_to_node()
-      end, vim.tbl_extend("force", opts, { desc = "Change dir to current dir" }))
+      end, opts("Change dir to current dir"))
 
       vim.keymap.set("n", "<leader>p", function()
         api.tree.change_root_to_parent()
-      end, vim.tbl_extend("force", opts, { desc = "Change dir to parent directory" }))
+      end, opts("Change dir to parent directory"))
     end
+
     -- configure nvim-tree
     nvimtree.setup({
-      on_attach = on_attach,
+      on_attach = my_on_attach,
       view = {
         width = 35,
         relativenumber = true,
@@ -66,13 +69,11 @@ return {
         ignore = false,
       },
     })
-
-    -- set keymaps
+    -- set global keymaps for nvim-tree
     local keymap = vim.keymap
-
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end,
 }
